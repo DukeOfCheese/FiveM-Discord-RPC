fb20083c373d5cc78ded881a19eeb2b620a69a4a
DukeOfCheese <81943364+DukeOfCheese@users.noreply.github.com>
2024-10-26 15:45:51 +1100

    "Added local variables and functions at the top of the script, refactored vehicle type checks, and updated Discord rich presence text to include vehicle type and speed."

--- AtlasDiscord/discord.lua
+++ AtlasDiscord/discord.lua
@@ -3,38 +3,61 @@
 ---------------------------------------------
 ------Editing this file may cause issues-----
 ------------Use config.lua instead-----------
+---------------------------------------------
+local player = GetPlayerPed(-1)
+local index = GetPlayerIndex(player)
+local id = GetPlayerServerId(PlayerId())
+local player_name = GetPlayerName(index)
+local driveable = {0, 2, 3, 5, 6, 7, 11, 12}
+local flyable = {1, 8, 9, 10}
+local sail = {13, 15}
+
+local function tableContains(tbl, val)
+    for _, v in ipairs(tbl) do
+        if v == val then
+            return true
+        end
+    end
+    return false
+end
 
 Citizen.CreateThread(function(source)
-    local player = GetPlayerPed(-1)
-    local index = GetPlayerIndex(player)
-    local id = GetPlayerServerId(PlayerId())
-    local player_name = GetPlayerName(index)
     while true do
-        -- DO NOT TOUCH THIS SECTION, it makes sure your game doesn't crash and is getting variables correctly
-        -- If you do know what you are doing, feel free to make a pull request
+        -- DO NOT TOUCH THIS SECTION, it ensures your game doesn't crash and retrieves variables correctly
         local player_count = #GetActivePlayers()
         local start_fps = GetFrameCount()
-        Citizen.Wait(5*1000)
+        Citizen.Wait(5 * 1000)
         local end_fps = GetFrameCount()
         local fps = math.floor((end_fps - start_fps) / 5)
+        local player = GetPlayerPed(-1)
+        local vehicle = GetVehiclePedIsIn(player)
 
-        -- The values below are controlled by config.lua, so please edit them there
-        -- NO NEED TO EDIT - USE CONFIG.LUA
+        -- Configuration-based Discord presence setup
         SetDiscordAppId(Config.AppID)
-        SetRichPresence("Players: ".. player_count .. "/".. Config.maxplayers)
+        SetRichPresence("Players: " .. player_count .. "/" .. Config.maxplayers)
         SetDiscordRichPresenceAsset("big")
-        if (IsPedInAnyVehicle(GetPlayerPed(-1), false)) then
-            local speed = GetEntitySpeed(GetVehiclePedIsIn(GetPlayerPed(-1), false))*2.2369
-            SetDiscordRichPresenceAssetText("Player is driving on " .. GetStreetNameFromHashKey(GetStreetNameAtCoord(table.unpack(GetEntityCoords(player)))) .. " at " .. math.floor(speed) .. " mph")
-        elseif not (IsPedInAnyVehicle(GetPlayerPed(-1), false)) then
+
+        if vehicle then
+            local speed = GetEntitySpeed(vehicle) * 2.2369 -- Convert m/s to mph
+            local vehicleType = GetVehicleTypeRaw(vehicle)
+            local streetName = GetStreetNameFromHashKey(GetStreetNameAtCoord(table.unpack(GetEntityCoords(player))))
+
+            if tableContains(driveable, vehicleType) then
+                SetDiscordRichPresenceAssetText("Player is driving on " .. streetName .. " at " .. math.floor(speed) .. " mph")
+            elseif tableContains(flyable, vehicleType) then
+                SetDiscordRichPresenceAssetText("Player is flying on " .. streetName .. " at " .. math.floor(speed) .. " mph")
+            elseif tableContains(sail, vehicleType) then
+                SetDiscordRichPresenceAssetText("Player is sailing on " .. streetName .. " at " .. math.floor(speed) .. " mph")
+            end
+        else
             SetDiscordRichPresenceAssetText("Player is walking on " .. GetStreetNameFromHashKey(GetStreetNameAtCoord(table.unpack(GetEntityCoords(player)))))
         end
+
         SetDiscordRichPresenceAssetSmall("small")
-        SetDiscordRichPresenceAssetSmallText("ID: ".. id .. " | " .. "FPS: " .. fps .. " fps")
+        SetDiscordRichPresenceAssetSmallText("ID: " .. id .. " | FPS: " .. fps .. " fps")
 
         SetDiscordRichPresenceAction(0, Config.button1_text, Config.button1_link)
         SetDiscordRichPresenceAction(1, Config.button2_text, Config.button2_link)
-
     end
 end)
 
